5b04c6b71cc685a334183d8f5fe5acf0d4a44872
josiasFraga <josias@zapshop.com.br>
2023-05-31 10:03:56 -0300

    change colector positions

--- src/Pages/DashboardPage/components/RepresentacaoCaminhaoGrande.js
+++ src/Pages/DashboardPage/components/RepresentacaoCaminhaoGrande.js
@@ -9,49 +9,49 @@ export default function RepresentacaoCaminhaoGrande(props) {
   return (
     <div style={{position: 'relative', margin: '0 auto', width: 600}}>
     <img src='/truck.png' width={600} style={{position: 'absolute', top: 40, left: 0}} />
-    <img src={car_images[1]} width={100} style={{
+    <img src={car_images[10]} width={100} style={{
       position: 'absolute', 
       top: 40, 
       left: 25,
       transform: 'rotate(5deg)'
     }} />
-    <img src={car_images[2]} width={105} style={{
+    <img src={car_images[11]} width={105} style={{
       position: 'absolute', 
       top: 42, 
       left: 129, 
       transform: 'scaleX(-1)'
     }} />
-    <img src={car_images[3]} width={100} style={{
+    <img src={car_images[1]} width={100} style={{
       position: 'absolute', 
       top: 35, 
       left: 249,
       transform: 'rotate(6deg)'
     }} />
-    <img src={car_images[4]} width={120} style={{
+    <img src={car_images[2]} width={120} style={{
       position: 'absolute', 
       top: 30, 
       left: 360, 
       transform: 'scaleX(-1) rotate(2deg)',
     }} />
-    <img src={car_images[5]} width={110} style={{
+    <img src={car_images[3]} width={110} style={{
       position: 'absolute', 
       top: 37, 
       left: 490,
       transform: 'rotate(7deg)'
     }} />
-    <img src={car_images[6]} width={113} style={{
+    <img src={car_images[4]} width={113} style={{
       position: 'absolute', 
       top: 90, 
       left: 79,
       transform: 'rotate(2deg)'
     }} />
-    <img src={car_images[7]} width={110} style={{
+    <img src={car_images[5]} width={110} style={{
       position: 'absolute', 
       top: 80, 
       left: 220,
       transform: 'rotate(9deg)'
     }} />
-    <img src={car_images[8]} width={110} style={{
+    <img src={car_images[7]} width={110} style={{
       position: 'absolute', 
       top: 75, 
       left: 355,
@@ -63,13 +63,13 @@ export default function RepresentacaoCaminhaoGrande(props) {
       left: 470,
       transform: 'rotate(5deg)'
     }} />
-    <img src={car_images[10]} width={100} style={{
+    <img src={car_images[6]} width={100} style={{
       position: 'absolute', 
       top: 120, 
       left: 195, 
       transform: 'scaleX(-1)'
     }} />
-    <img src={car_images[11]} width={110} style={{
+    <img src={car_images[8]} width={110} style={{
       position: 'absolute', 
       top: 115, 
       left: 325, 
--- src/Pages/DashboardPage/components/RepresentacaoCaminhaoPequena.js
+++ src/Pages/DashboardPage/components/RepresentacaoCaminhaoPequena.js
@@ -9,49 +9,49 @@ export default function RepresentacaoCaminhaoPequena(props) {
   return (
     <div style={{position: 'relative', margin: '0 auto', width: 300}}>
     <img src='/truck.png' width={300} style={{position: 'absolute', top: 65, left: 0}} />
-    <img src={car_images[1]} width={50} style={{
+    <img src={car_images[10]} width={50} style={{
       position: 'absolute', 
       top: 67, 
       left: 9,
       transform: 'rotate(5deg)'
     }} />
-    <img src={car_images[2]} width={52.5} style={{
+    <img src={car_images[11]} width={52.5} style={{
       position: 'absolute', 
       top: 65, 
       left: 65, 
       transform: 'scaleX(-1)'
     }} />
-    <img src={car_images[3]} width={50} style={{
+    <img src={car_images[1]} width={50} style={{
       position: 'absolute', 
       top: 62, 
       left: 123,
       transform: 'rotate(6deg)'
     }} />
-    <img src={car_images[4]} width={60} style={{
+    <img src={car_images[2]} width={60} style={{
       position: 'absolute', 
       top: 60, 
       left: 180, 
       transform: 'scaleX(-1) rotate(2deg)',
     }} />
-    <img src={car_images[5]} width={55} style={{
+    <img src={car_images[3]} width={55} style={{
       position: 'absolute', 
       top: 65, 
       left: 246,
       transform: 'rotate(7deg)'
     }} />
-    <img src={car_images[6]} width={56.5} style={{
+    <img src={car_images[4]} width={56.5} style={{
       position: 'absolute', 
       top: 90, 
       left: 38,
       transform: 'rotate(2deg)'
     }} />
-    <img src={car_images[7]} width={55} style={{
+    <img src={car_images[5]} width={55} style={{
       position: 'absolute', 
       top: 85, 
       left: 97,
       transform: 'rotate(9deg)'
     }} />
-    <img src={car_images[8]} width={55} style={{
+    <img src={car_images[7]} width={55} style={{
       position: 'absolute', 
       top: 82, 
       left: 178,
@@ -63,13 +63,13 @@ export default function RepresentacaoCaminhaoPequena(props) {
       left: 236,
       transform: 'rotate(5deg)'
     }} />
-    <img src={car_images[10]} width={50} style={{
+    <img src={car_images[6]} width={50} style={{
       position: 'absolute', 
       top: 104, 
       left: 99, 
       transform: 'scaleX(-1)'
     }} />
-    <img src={car_images[11]} width={55} style={{
+    <img src={car_images[8]} width={55} style={{
       position: 'absolute', 
       top: 102, 
       left: 162, 
