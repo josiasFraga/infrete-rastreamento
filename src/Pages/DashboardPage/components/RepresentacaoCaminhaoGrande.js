83f3e516e56f461ecd4cc00be5a51b5d0133b780
josiasFraga <josias@zapshop.com.br>
2023-09-13 17:57:43 -0300

    updates

--- src/Pages/DashboardPage/components/RepresentacaoCaminhaoGrande.js
+++ src/Pages/DashboardPage/components/RepresentacaoCaminhaoGrande.js
@@ -9,24 +9,24 @@ export default function RepresentacaoCaminhaoGrande(props) {
 
   return (
     <div style={{position: 'relative', margin: '0 auto', width: 600}}>
-    <img src='/truck.png' width={600} style={{position: 'absolute', top: 40, left: 0}} />
+    <img src='/truck.png' width={600} style={{position: 'absolute', top: 20, left: 0}} />
   
-    <div style={{ position: 'absolute', top: 40, left: 25 }}>
-      <img src={car_images[1]} width={100} style={{ transform: 'rotate(5deg)', opacity: car_images[1] === 'car.png' || car_images[1] === 'car_disabled.png' ? opacity_standard : 1 }} />
+    <div style={{ position: 'absolute', top: 23, left: 82 }}>
+      <img src={car_images[1]} width={100} style={{ transform: 'rotate(11deg)', opacity: car_images[1] === 'car.png' || car_images[1] === 'car_disabled.png' ? opacity_standard : 1 }} />
       <span style={{ position: 'absolute', top: '40%', left: '50%', transform: 'translate(-50%, -50%)', background: 'rgba(0, 0, 0, 0.5)', color: 'white', padding: '4px 8px' }}>1</span>
     </div>
 
-    <div style={{ position: 'absolute', top: 42, left: 129 }}>
+    <div style={{ position: 'absolute', top: 28, left: 185 }}>
       <img src={car_images[2]} width={105} style={{ transform: 'scaleX(-1)', opacity: car_images[2] === 'car.png' || car_images[2] === 'car_disabled.png' ? opacity_standard : 1 }} />
       <span style={{ position: 'absolute', top: '40%', left: '50%', transform: 'translate(-50%, -50%)', background: 'rgba(0, 0, 0, 0.5)', color: 'white', padding: '4px 8px' }}>2</span>
     </div>
 
-    <div style={{ position: 'absolute', top: 35, left: 249 }}>
-      <img src={car_images[3]} width={100} style={{ transform: 'rotate(6deg)', opacity: car_images[3] === 'car.png' || car_images[3] === 'car_disabled.png' ? opacity_standard : 1 }} />
+    <div style={{ position: 'absolute', top: 30, left: 290 }}>
+      <img src={car_images[3]} width={100} style={{ transform: 'rotate(1deg)', opacity: car_images[3] === 'car.png' || car_images[3] === 'car_disabled.png' ? opacity_standard : 1 }} />
       <span style={{ position: 'absolute', top: '40%', left: '50%', transform: 'translate(-50%, -50%)', background: 'rgba(0, 0, 0, 0.5)', color: 'white', padding: '4px 8px' }}>3</span>
     </div>
 
-    <div style={{ position: 'absolute', top: 30, left: 360 }}>
+    <div style={{ position: 'absolute', top: 30, left: 390 }}>
       <img src={car_images[4]} width={100} style={{ transform: 'scaleX(-1) rotate(2deg)', opacity: car_images[4] === 'car.png' || car_images[4] === 'car_disabled.png' ? opacity_standard : 1 }} />
       <span style={{ position: 'absolute', top: '40%', left: '50%', transform: 'translate(-50%, -50%)', background: 'rgba(0, 0, 0, 0.5)', color: 'white', padding: '4px 8px' }}>4</span>
     </div>
@@ -36,32 +36,32 @@ export default function RepresentacaoCaminhaoGrande(props) {
       <span style={{ position: 'absolute', top: '40%', left: '50%', transform: 'translate(-50%, -50%)', background: 'rgba(0, 0, 0, 0.5)', color: 'white', padding: '4px 8px' }}>5</span>
     </div>
 
-    <div style={{ position: 'absolute', top: 90, left: 79 }}>
+    <div style={{ position: 'absolute', top: 88, left: 118 }}>
       <img src={car_images[6]} width={113} style={{ transform: 'rotate(2deg)', opacity: car_images[6] === 'car.png' || car_images[6] === 'car_disabled.png' ? opacity_standard : 1 }} />
       <span style={{ position: 'absolute', top: '40%', left: '50%', transform: 'translate(-50%, -50%)', background: 'rgba(0, 0, 0, 0.5)', color: 'white', padding: '4px 8px' }}>6</span>
     </div>
 
-    <div style={{ position: 'absolute', top: 80, left: 220 }}>
-      <img src={car_images[8]} width={110} style={{ transform: 'rotate(9deg)', opacity: car_images[8] === 'car.png' || car_images[8] === 'car_disabled.png' ? opacity_standard : 1 }} />
+    <div style={{ position: 'absolute', top: 74, left: 224 }}>
+      <img src={car_images[8]} width={110} style={{ transform: 'rotate(6deg)', opacity: car_images[8] === 'car.png' || car_images[8] === 'car_disabled.png' ? opacity_standard : 1 }} />
       <span style={{ position: 'absolute', top: '40%', left: '50%', transform: 'translate(-50%, -50%)', background: 'rgba(0, 0, 0, 0.5)', color: 'white', padding: '4px 8px' }}>8</span>
     </div>
 
     <div style={{ position: 'absolute', top: 75, left: 355 }}>
-      <img src={car_images[10]} width={110} style={{ transform: 'rotate(9deg)', opacity: car_images[10] === 'car.png' || car_images[10] === 'car_disabled.png' ? opacity_standard : 1 }} />
+      <img src={car_images[10]} width={110} style={{ transform: 'rotate(6deg)', opacity: car_images[10] === 'car.png' || car_images[10] === 'car_disabled.png' ? opacity_standard : 1 }} />
       <span style={{ position: 'absolute', top: '40%', left: '50%', transform: 'translate(-50%, -50%)', background: 'rgba(0, 0, 0, 0.5)', color: 'white', padding: '4px 8px' }}>10</span>
     </div>
 
-    <div style={{ position: 'absolute', top: 85, left: 470 }}>
-      <img src={car_images[11]} width={120} style={{ transform: 'rotate(5deg)', opacity: car_images[11] === 'car.png' || car_images[11] === 'car_disabled.png' ? opacity_standard : 1 }} />
+    <div style={{ position: 'absolute', top: 98, left: 482 }}>
+      <img src={car_images[11]} width={120} style={{ transform: 'rotate(9deg)', opacity: car_images[11] === 'car.png' || car_images[11] === 'car_disabled.png' ? opacity_standard : 1 }} />
       <span style={{ position: 'absolute', top: '40%', left: '50%', transform: 'translate(-50%, -50%)', background: 'rgba(0, 0, 0, 0.5)', color: 'white', padding: '4px 8px' }}>11</span>
     </div>
 
-    <div style={{ position: 'absolute', top: 120, left: 195 }}>
+    <div style={{ position: 'absolute', top: 120, left: 218 }}>
       <img src={car_images[7]} width={120} style={{ transform: 'scaleX(-1)', opacity: car_images[7] === 'car.png' || car_images[7] === 'car_disabled.png' ? opacity_standard : 1 }} />
       <span style={{ position: 'absolute', top: '40%', left: '50%', transform: 'translate(-50%, -50%)', background: 'rgba(0, 0, 0, 0.5)', color: 'white', padding: '4px 8px' }}>7</span>
     </div>
 
-    <div style={{ position: 'absolute', top: 115, left: 325 }}>
+    <div style={{ position: 'absolute', top: 120, left: 332 }}>
       <img src={car_images[9]} width={120} style={{ transform: 'scaleX(-1)', opacity: car_images[9] === 'car.png' || car_images[9] === 'car_disabled.png' ? opacity_standard : 1 }} />
       <span style={{ position: 'absolute', top: '40%', left: '50%', transform: 'translate(-50%, -50%)', background: 'rgba(0, 0, 0, 0.5)', color: 'white', padding: '4px 8px' }}>9</span>
     </div>
--- src/Pages/DashboardPage/components/RepresentacaoCaminhaoPequena.js
+++ src/Pages/DashboardPage/components/RepresentacaoCaminhaoPequena.js
@@ -9,59 +9,59 @@ export default function RepresentacaoCaminhaoPequena(props) {
 
   return (
     <div style={{position: 'relative', margin: '0 auto', width: 300}}>
-    <img src='/truck.png' width={300} style={{position: 'absolute', top: 65, left: 0}} />
+    <img src='/truck.png' width={300} style={{position: 'absolute', top: 57, left: 0}} />
 
-    <div style={{ position: 'absolute', top: 67, left: 9 }}>
-      <img src={car_images[1]} width={50} style={{ transform: 'rotate(5deg)', opacity: car_images[1] === 'car.png' || car_images[1] === "car_disabled.png" ? opacity_standard : 1 }} />
+    <div style={{ position: 'absolute', top: 58, left: 41 }}>
+      <img src={car_images[1]} width={50} style={{ transform: 'rotate(10deg)', opacity: car_images[1] === 'car.png' || car_images[1] === "car_disabled.png" ? opacity_standard : 1 }} />
       <span style={{ position: 'absolute', top: '40%', left: '50%', transform: 'translate(-50%, -50%)', background: 'rgba(0, 0, 0, 0.5)', color: 'white', padding: '0px 3px' }}>1</span>
     </div>
 
-    <div style={{ position: 'absolute', top: 65, left: 65 }}>
+    <div style={{ position: 'absolute', top: 62, left: 90 }}>
       <img src={car_images[2]} width={52.5} style={{ transform: 'scaleX(-1)', opacity: car_images[2] === 'car.png' || car_images[2] === "car_disabled.png" ? opacity_standard : 1 }} />
       <span style={{ position: 'absolute', top: '40%', left: '50%', transform: 'translate(-50%, -50%)', background: 'rgba(0, 0, 0, 0.5)', color: 'white', padding: '0px 3px' }}>2</span>
     </div>
 
-    <div style={{ position: 'absolute', top: 62, left: 123 }}>
-      <img src={car_images[3]} width={50} style={{ transform: 'rotate(6deg)', opacity: car_images[3] === 'car.png' || car_images[3] === "car_disabled.png" ? opacity_standard : 1 }} />
+    <div style={{ position: 'absolute', top: 62, left: 140 }}>
+      <img src={car_images[3]} width={50} style={{ transform: 'rotate(1deg)', opacity: car_images[3] === 'car.png' || car_images[3] === "car_disabled.png" ? opacity_standard : 1 }} />
       <span style={{ position: 'absolute', top: '40%', left: '50%', transform: 'translate(-50%, -50%)', background: 'rgba(0, 0, 0, 0.5)', color: 'white', padding: '0px 3px' }}>3</span>
     </div>
 
-    <div style={{ position: 'absolute', top: 60, left: 180 }}>
-      <img src={car_images[4]} width={60} style={{ transform: 'scaleX(-1) rotate(2deg)', opacity: car_images[4] === 'car.png' || car_images[4] === "car_disabled.png" ? opacity_standard : 1 }} />
+    <div style={{ position: 'absolute', top: 58, left: 190 }}>
+      <img src={car_images[4]} width={60} style={{ transform: 'rotate(0deg)', opacity: car_images[4] === 'car.png' || car_images[4] === "car_disabled.png" ? opacity_standard : 1 }} />
       <span style={{ position: 'absolute', top: '40%', left: '50%', transform: 'translate(-50%, -50%)', background: 'rgba(0, 0, 0, 0.5)', color: 'white', padding: '0px 3px' }}>4</span>
     </div>
 
-    <div style={{ position: 'absolute', top: 65, left: 246 }}>
+    <div style={{ position: 'absolute', top: 65, left: 249 }}>
       <img src={car_images[5]} width={55} style={{ transform: 'rotate(7deg)', opacity: car_images[5] === 'car.png' || car_images[5] === "car_disabled.png" ? opacity_standard : 1 }} />
       <span style={{ position: 'absolute', top: '40%', left: '50%', transform: 'translate(-50%, -50%)', background: 'rgba(0, 0, 0, 0.5)', color: 'white', padding: '0px 3px' }}>5</span>
     </div>
 
-    <div style={{ position: 'absolute', top: 90, left: 38 }}>
+    <div style={{ position: 'absolute', top: 91, left: 65 }}>
       <img src={car_images[6]} width={56.5} style={{ transform: 'rotate(2deg)', opacity: car_images[6] === 'car.png' || car_images[6] === "car_disabled.png" ? opacity_standard : 1 }} />
       <span style={{ position: 'absolute', top: '40%', left: '50%', transform: 'translate(-50%, -50%)', background: 'rgba(0, 0, 0, 0.5)', color: 'white', padding: '0px 3px' }}>6</span>
     </div>
 
-    <div style={{ position: 'absolute', top: 85, left: 97 }}>
+    <div style={{ position: 'absolute', top: 85, left: 119 }}>
       <img src={car_images[8]} width={55} style={{ transform: 'rotate(9deg)', opacity: car_images[8] === 'car.png' || car_images[8] === "car_disabled.png" ? opacity_standard : 1 }} />
       <span style={{ position: 'absolute', top: '40%', left: '50%', transform: 'translate(-50%, -50%)', background: 'rgba(0, 0, 0, 0.5)', color: 'white', padding: '0px 3px' }}>8</span>
     </div>
 
-    <div style={{ position: 'absolute', top: 82, left: 178 }}>
-      <img src={car_images[10]} width={55} style={{ transform: 'rotate(9deg)', opacity: car_images[10] === 'car.png' || car_images[10] === "car_disabled.png" ? opacity_standard : 1 }} />
+    <div style={{ position: 'absolute', top: 85, left: 180 }}>
+      <img src={car_images[10]} width={55} style={{ transform: 'rotate(5deg)', opacity: car_images[10] === 'car.png' || car_images[10] === "car_disabled.png" ? opacity_standard : 1 }} />
       <span style={{ position: 'absolute', top: '40%', left: '50%', transform: 'translate(-50%, -50%)', background: 'rgba(0, 0, 0, 0.5)', color: 'white', padding: '0px 3px' }}>10</span>
     </div>
 
-    <div style={{ position: 'absolute', top: 87, left: 236 }}>
+    <div style={{ position: 'absolute', top: 95, left: 238 }}>
       <img src={car_images[11]} width={60} style={{ transform: 'rotate(5deg)', opacity: car_images[11] === 'car.png' || car_images[11] === "car_disabled.png" ? opacity_standard : 1 }} />
       <span style={{ position: 'absolute', top: '40%', left: '50%', transform: 'translate(-50%, -50%)', background: 'rgba(0, 0, 0, 0.5)', color: 'white', padding: '0px 3px' }}>11</span>
     </div>
 
-    <div style={{ position: 'absolute', top: 104, left: 99 }}>
+    <div style={{ position: 'absolute', top: 110, left: 114 }}>
       <img src={car_images[7]} width={50} style={{ transform: 'scaleX(-1)', opacity: car_images[7] === 'car.png' || car_images[7] === "car_disabled.png" ? opacity_standard : 1 }} />
       <span style={{ position: 'absolute', top: '40%', left: '50%', transform: 'translate(-50%, -50%)', background: 'rgba(0, 0, 0, 0.5)', color: 'white', padding: '0px 3px' }}>7</span>
     </div>
 
-    <div style={{ position: 'absolute', top: 102, left: 162 }}>
+    <div style={{ position: 'absolute', top: 109, left: 162 }}>
       <img src={car_images[9]} width={55} style={{ transform: 'scaleX(-1)', opacity: car_images[9] === 'car.png' || car_images[9] === "car_disabled.png" ? opacity_standard : 1 }} />
       <span style={{ position: 'absolute', top: '40%', left: '50%', transform: 'translate(-50%, -50%)', background: 'rgba(0, 0, 0, 0.5)', color: 'white', padding: '0px 3px' }}>9</span>
     </div>
